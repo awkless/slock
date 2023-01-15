02f555589c39c13869360dc984d2633c353571fd
Jason Pena <jasonpena@awkless.com>
2023-01-15 16:17:54 -0700

    feat(patch): Add colormessage patch
    Applied colormessage patch by Guy Shefy <guyshefyb@gmail.com>, which
    allows you to produce messages in slock but with color[1].
    
    [1]: https://tools.suckless.org/slock/patches/colormessage/
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
+static const char *user  = "nouser";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
@@ -10,3 +10,12 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less!";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x13";
